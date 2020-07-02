382a9ff68f61a72653ef84616bf893e673eea6f5
daniel martins <freitasdaniel39@gmail.com>
2020-07-02 19:49:42 -0300

    adicionando dialogflow ao projeto

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const token = '1326050646:AAEA0pLp45K4G5tTY36ri9ZiC38H4HEw9C4';
 
 const bot = new TelegramBot(token, { polling: true });
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg.text);
+    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
+});
