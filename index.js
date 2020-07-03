5b67549fd264b85ef968dd429463486984f8c56d
daniel martins <freitasdaniel39@gmail.com>
2020-07-03 10:42:27 -0300

    configurando integração com dialogflow

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api');
+const dialogflow = require('./dialogflow');
 
 const token = '1326050646:AAEA0pLp45K4G5tTY36ri9ZiC38H4HEw9C4';
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     console.log(msg.text);
-    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
+
+    const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
+
+    if(dfResponse.intent === 'Treino especifico') {
+        
+    }
+
+    bot.sendMessage(chatId, dfResponse.text);
 });
--- youtube.js
+++ youtube.js
@@ -4,9 +4,13 @@ const config = require('./yt-config.json');
 const youtube = new Youtube();
 youtube.setKey(config.key);
 
-youtube.search('Exercicio em casa para biceps', 2, (error, result) => {
-    if (!error)
-        console.log(JSON.stringify(result, null, 2));
-    else
-    console.log('Deu erro');
-});
+function searchVideoUrl(message, queryText) {
+    return new Promise((resolve, reject) => {
+        youtube.search(`Exercicio em casa para biceps ${queryText}`, 2, (error, result) => {
+            if (!error)
+                console.log(JSON.stringify(result, null, 2));
+            else
+                console.log('Deu erro');
+        });
+    });
+}
